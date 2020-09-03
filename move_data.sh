1fc08afbe513a7238fe112d87e46ace4d5a6bb16
joycenerd <joycenerd@csie.io>
2020-09-03 20:33:44 +0800

    move train and valid

---
+++ move_data.sh
@@ -0,0 +1,14 @@
+mkdir ./aoi/Data
+mkdir ./aoi/Data/0 ./aoi/Data/1 ./aoi/Data/2 ./aoi/Data/3 ./aoi/Data/4 ./aoi/Data/5
+cp -ar ./aoi/train_images/0/train ./aoi/Data/0  
+cp -ar ./aoi/train_images/0/valid ./aoi/Data/0
+cp -ar ./aoi/train_images/1/train ./aoi/Data/1
+cp -ar ./aoi/train_images/1/valid ./aoi/Data/1
+cp -ar ./aoi/train_images/2/train ./aoi/Data/2
+cp -ar ./aoi/train_images/2/valid ./aoi/Data/2
+cp -ar ./aoi/train_images/3/train ./aoi/Data/3
+cp -ar ./aoi/train_images/3/valid ./aoi/Data/3
+cp -ar ./aoi/train_images/4/train ./aoi/Data/4
+cp -ar ./aoi/train_images/4/valid ./aoi/Data/4
+cp -ar ./aoi/train_images/5/train ./aoi/Data/5
+cp -ar ./aoi/train_images/5/valid ./aoi/Data/5
